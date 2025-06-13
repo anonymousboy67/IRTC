54315d30d9caca79a73e6c934337efc7acf323bd
anonymousboy67 <aashishad67@gmail.com>
2025-06-13 05:49:35 +0545

    working in features

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,10 +1,21 @@
 
 package org.example;
 
+import org.example.services.UserBookingService;
+
+import java.util.Scanner;
+
 public class App {
 
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int option=0;
+        UserBookingService userBookingService;
+        try{
+            userBookingService=new UserBookingService();
+        }catch ()
 
     };
 
